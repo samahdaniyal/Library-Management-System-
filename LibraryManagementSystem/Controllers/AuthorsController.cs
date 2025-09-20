b25087fa67712e48dff97f9ad39a3d76268e193a
samahdaniyal <122165292+samahdaniyal@users.noreply.github.com>
2025-09-20 20:22:29 +0500

    Part 3: Controller
    Added Controller Changes in BooksController and AuthorsController

--- LibraryManagementSystem/Controllers/AuthorsController.cs
+++ LibraryManagementSystem/Controllers/AuthorsController.cs
@@ -12,7 +12,11 @@ public class AuthorsController(LibraryContext context) : Controller
         // DO NOT MODIFY ABOVE THIS LINE
         // TODO: 11.1 Fetch all authors and return list, include Books for each author and return the view with authors
         // Refer to similar listing for Members
-        throw new NotImplementedException("AuthorsController.Authors is not implemented");
+        // throw new NotImplementedException("AuthorsController.Authors is not implemented");
+        var authors = context.Authors
+            .Include(a => a.Books)
+            .ToList();
+        return View(authors);
         // DO NOT MODIFY BELOW THIS LINE
     }
 
@@ -27,9 +31,15 @@ public class AuthorsController(LibraryContext context) : Controller
     {
         // DO NOT MODIFY ABOVE THIS LINE
         // TODO: 11.2 Check if model is valid then add author to context and save changes, then redirect to Authors action
-        
+        if (ModelState.IsValid)
+        {
+            context.Authors.Add(author);
+            context.SaveChanges();
+            return RedirectToAction("Authors");
+        }
         // TODO: 11.3 Return the view with author if model is not valid, errors will be auto populated by the framework
-        throw new NotImplementedException("AuthorsController.Add is not implemented");
+        // throw new NotImplementedException("AuthorsController.Add is not implemented");
+        return View(author);
         // DO NOT MODIFY BELOW THIS LINE
     }
 
@@ -38,9 +48,19 @@ public class AuthorsController(LibraryContext context) : Controller
     {
         // DO NOT MODIFY ABOVE THIS LINE
         // TODO: 11.4 Check if author exists, remove author from context and save changes, then redirect to Authors action
-        
+        // Checking author exists
+        var author = context.Authors.Find(id);
+        if (author != null)
+        {
+            // Removing author from context and saving changes
+            context.Authors.Remove(author);
+            context.SaveChanges();
+            return RedirectToAction("Authors");
+        }
         // TODO: 11.5 Return NotFound() if author does not exist
-        throw new NotImplementedException("AuthorsController.Delete is not implemented");
+        // throw new NotImplementedException("AuthorsController.Delete is not implemented");
+        // Returning NotFound() if author does not exist
+        return NotFound();
         // DO NOT MODIFY BELOW THIS LINE
     }
 
@@ -49,7 +69,17 @@ public class AuthorsController(LibraryContext context) : Controller
     {
         // DO NOT MODIFY ABOVE THIS LINE
         // TODO: 11.6 Find author by id, return NotFound() if author does not exist, otherwise return the view with author
-        throw new NotImplementedException("AuthorsController.Update is not implemented");
+        // throw new NotImplementedException("AuthorsController.Update is not implemented");
+        // Finding author by id
+        var author = context.Authors.Find(id);
+        if (author == null)
+        {
+            // Returning NotFound() if author does not exist
+            return NotFound();
+        }
+        
+        // Returning the view with author
+        return View(author);
         // DO NOT MODIFY BELOW THIS LINE
     }
 
@@ -58,7 +88,17 @@ public class AuthorsController(LibraryContext context) : Controller
     {
         // DO NOT MODIFY ABOVE THIS LINE
         // TODO: 11.7 Check if model is valid then update author in context and save changes, then redirect to Authors action
-        throw new NotImplementedException("AuthorsController.Update is not implemented");
+        // throw new NotImplementedException("AuthorsController.Update is not implemented");
+        // Checking if model is valid then updating author in context and saving changes
+        if (ModelState.IsValid)
+        {
+            context.Authors.Update(author);
+            context.SaveChanges();
+            return RedirectToAction("Authors");
+        }
+        
+        // Returning view with validation errors if model is not valid
+        return View(author);
         // DO NOT MODIFY BELOW THIS LINE
     }
 }
